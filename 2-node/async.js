ed41ffb65b62b4c323b8624e614b962a88573e65
Leon <leonrubner2809@gmail.com>
2020-11-16 19:24:10 +0100

    write and end webstorm errors???

--- 2-node/async.js
+++ 2-node/async.js
@@ -1,6 +1,22 @@
+const fetchData = ( ) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        }, 1500);
+    });
+}
+
 setTimeout(() => {
     console.log('Timer is done');
-}, 2);
+    fetchData()
+        .then(text => {
+            console.log(text);
+            return fetchData();
+    })
+        .then(text2 => {
+            console.log(text2);
+        })
+}, 2000);
 
 console.log('Hello');
 console.log('Hi');
