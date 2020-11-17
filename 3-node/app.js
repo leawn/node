52f930afb77fd374cbfa65a5a8c183d468afe5a7
Leon <leonrubner2809@gmail.com>
2020-11-17 10:50:59 +0100

    node

--- 3-node/app.js
+++ 3-node/app.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    /*console.log(req.url, req.method, req.headers);*/
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html lang="en">');
@@ -12,10 +12,19 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
-    //process.exit();
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.writeHead(302, 'Location', '/');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
