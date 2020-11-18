8f38ab8db7e1aaf7b7cc2813b9e74599af4b7f80
Leon <leonrubner2809@gmail.com>
2020-11-18 12:27:38 +0100

    util dir added html files added inside of views dir

--- 4-node-express/app.js
+++ 4-node-express/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(adminRoutes);
+app.use('/admin' ,adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+})
 
 app.listen(3000);
