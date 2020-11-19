6f1593e5e6ddd67d3d96343b53192fdec41b85e4
Leon <leonrubner2809@gmail.com>
2020-11-19 14:16:47 +0100

    template engine comparison

--- 4-node-express/app.js
+++ 4-node-express/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+app.set('view engine', 'handlebars');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin' ,adminRoutes);
+app.use('/admin' ,adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    res.status(404).render('not-found-handlebars', {pageTitle: 'Not found 404'})
 })
 
 app.listen(3000);
--- 4-node-express/routes/shop.js
+++ 4-node-express/routes/shop.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop-handlebars', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
