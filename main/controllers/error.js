573b4d9167205c6f1411dfe1ccc26e83de9715f3
Leon <leonrubner2809@gmail.com>
2020-11-29 13:57:40 +0100

    some changes

--- main/controllers/error.js
+++ main/controllers/error.js
@@ -4,6 +4,6 @@ exports.getNotFound = (req, res, next) => {
         activeShop: false,
         formsCss: false,
         productCss: false,
-        path: undefined
+        path: '/404'
     });
 }
