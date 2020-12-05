d2e58f28c9a9fa9fca469940c4598fa403007cc9
Leon <leonrubner2809@gmail.com>
2020-12-05 11:11:19 +0100

    right now it must work

--- main/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node_complete', 'leawn', 'Leonboss62!', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
